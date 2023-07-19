5bd20899cea82a6447481839184202dca710c5b0
WilliGomez <willgom.95@gmail.com>
2023-07-18 20:13:40 -0500

    Sera que si queda?

---
+++ script2.js
@@ -0,0 +1,34 @@
+/*const pokemonArray = [
+    "Bulbasaur",
+    "Ivysaur",
+    "Venusaur",
+    "Charmander",
+    "Charmeleon",
+    "Charizard",
+    "Squirtle",
+    "Wartortle",
+    "Blastoise",
+    "Caterpie",
+    "Metapod",
+    "Butterfree",
+    "Weedle",
+    "Kakuna",
+    "Beedrill",
+    "Pidgey",
+    "Pidgeotto",
+    "Pidgeot",
+    "Rattata",
+    "Raticate",
+    // ...otros Pokémons
+  ];
+  
+  // Solicitar al usuario un número
+  const numeroUsuario = parseInt(prompt("Ingresa un número:"));
+  
+  // Mostrar los nombres de los Pokémons cuyos números son múltiplos de 5
+  for (let i = 1; i <= numeroUsuario; i++) {
+    if (i % 5 === 0) {
+      console.log(pokemonArray[i - 1]);
+    }
+  }
+*/
